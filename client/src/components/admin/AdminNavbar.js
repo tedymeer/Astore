7cd2c2508960dc57da903692dff067681e6bcabb
taha mir <tahamir999@gmail.com>
2021-07-11 11:37:39 +0500

    minor changes

--- client/src/components/admin/AdminNavbar.js
+++ client/src/components/admin/AdminNavbar.js
@@ -9,13 +9,6 @@ function AdminNavbar() {
           <Nav className="mr-auto">
             <Nav.Link href="#features">View website</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
-            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown> */}
           </Nav>
           <Nav>
             <Nav.Link href="#deets">Hello Admin</Nav.Link>
