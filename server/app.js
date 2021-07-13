ca9d651714130a02e129486d1df9a7841a86cada
taha mir <tahamir999@gmail.com>
2021-07-14 00:37:09 +0500

    popop

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', adminRouter);
 
 app.listen(8000, (err) => { 
   console.log("running");
