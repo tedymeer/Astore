41433de596f4d17fad896a29360ae9fd5bb8eecb
taha mir <tahamir999@gmail.com>
2021-07-17 23:48:46 +0500

    pop

--- client/src/auth.js
+++ client/src/auth.js
@@ -15,7 +15,7 @@ class Auth {
     //     console.log(this.state.adminusername)
     //     console.log(this.state.adminpassword)
     //     console.log(this.state.authenticated)
-    axios.post('localhost:8000/login', {username:this.state.adminusername,password:this.state.adminpassword}, 
+    axios.post('http://localhost:8000/login', {username:this.state.adminusername,password:this.state.adminpassword}, 
     {withCredentials: true}).then(
         (res)=>{
           console.log(res.data)
