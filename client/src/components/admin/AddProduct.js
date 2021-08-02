f97031a8b901e58bf1b6c8d5779c67e74564ef9a
taha mir <tahamir999@gmail.com>
2021-08-02 20:12:26 +0500

    ok

--- client/src/components/admin/AddProduct.js
+++ client/src/components/admin/AddProduct.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Button } from 'react-bootstrap'
 
 function AddProduct() {
+    const [form, setForm] = useState({
+        prodId:null,
+        prodName:null,
+        prodPrice:null,
+        prodDes:null,
+        
+        prodCategory:null,
+
+        size:null,
+
+        Prodphotos:null
+
+    })
+    function handleChange(e){
+       
+       
+            setForm((oldVals)=>{
+                return{
+                    ...oldVals,
+                    [e.target.name]:e.target.value
+                }
+            })
+            
+     
+    }
     return (
         <div className="add-prod-wrap">
             <div className="add-prod-heading">Add a product</div>
@@ -10,33 +35,33 @@ function AddProduct() {
 
                <div className="prod-id-field">
                <label htmlFor="prodid">Product ID</label><br/>
-                <input id="prodid" type="text" name="prodId" className="field" />
+                <input id="prodid" type="text" name="prodId" className="field" onChange={handleChange}/>
                 </div>
                 <div className="prod-name-field">
                 <label htmlFor="prodname">Product Name</label><br/>    
-                <input id="prodname" type="text" name="prodName" className="field"/>
+                <input id="prodname" type="text" name="prodName" className="field" onChange={handleChange}/>
                 </div>
                 <div className="prod-price-field">
                 <label htmlFor="prodprice">Product Price</label><br/>
-                <input id="prodprice" type="number" name="prodPrice" min="0" step="any" className="field"/>
+                <input id="prodprice" type="number" name="prodPrice" min="0" step="any" className="field" onChange={handleChange}/>
                 
                 </div>
                 <div className="prod-discription-field">
                 <label htmlFor="discrip">Product Discription</label><br/>
-                    <textarea id="discrip" placeholder="Add a description" className="field"></textarea>
+                    <textarea name="prodDes" id="discrip" placeholder="Add a description" className="field" onChange={handleChange}></textarea>
                 </div>
                 <div className="prod-category-field">
                     <div>Select Category</div>
                 
-                <input type="radio" id="men" name="prodCategory" value="men"/> 
+                <input type="radio" id="men" name="prodCategory" value="men" onChange={handleChange}/> 
                 <label htmlFor="html"> Men</label>
                
                 
-                <input type="radio" id="women" name="prodCategory" value="women"/>
+                <input type="radio" id="women" name="prodCategory" value="women" onChange={handleChange}/>
                 <label htmlFor="women"> Women</label>
                 
                 
-                <input type="radio" id="kids" name="prodCategory" value="kids"/>
+                <input type="radio" id="kids" name="prodCategory" value="kids" onChange={handleChange}/>
                 <label htmlFor="kids"> Kids</label>
                 </div>
                 
@@ -45,38 +70,38 @@ function AddProduct() {
                   
                 <div> Select available sizes</div>
                 
-                <input type="checkbox" id="xxsmall" name="size" value="xxsmall"/>
+                <input type="checkbox" id="xxsmall" name="size" value="xxsmall" onChange={handleChange}/>
                 <label htmlFor="xxsmall"> xxsmall</label>
                 
                 
-                <input type="checkbox" id="xsmall" name="size" value="xsmall"/>
+                <input type="checkbox" id="xsmall" name="size" value="xsmall" onChange={handleChange}/>
                 <label htmlFor="xsmall"> xsmall</label>
                 
                 
-                <input type="checkbox" id="small" name="size" value="small"/>
+                <input type="checkbox" id="small" name="size" value="small" onChange={handleChange}/>
                 <label htmlFor="small"> small</label>
                 
                 
-                <input type="checkbox" id="medium" name="size" value="medium"/>
+                <input type="checkbox" id="medium" name="size" value="medium" onChange={handleChange}/>
                 <label htmlFor="medium"> medium</label>
                
                 
-                <input type="checkbox" id="large" name="size" value="large"/>
+                <input type="checkbox" id="large" name="size" value="large" onChange={handleChange}/>
                 <label htmlFor="large"> large</label>
                 
                 
-                <input type="checkbox" id="xlarge" name="size" value="xlarge"/>
+                <input type="checkbox" id="xlarge" name="size" value="xlarge" onChange={handleChange}/>
                 <label htmlFor="xlarge"> xlarge</label>
                
                  
-                <input type="checkbox" id="xxlarge" name="size" value="xxlarge"/>
+                <input type="checkbox" id="xxlarge" name="size" value="xxlarge" onChange={handleChange}/>
                 <label htmlFor="xxlarge"> xxlarge</label>
                         
                 
                 </div>
                 <div className="prod-photo-field">
                 <label htmlFor="photos">Select photos</label><br/>
-                <input type="file" id="photos" name="Prodphotos" multiple />
+                <input type="file" id="photos" name="Prodphotos" multiple onChange={handleChange}/>
                 </div>
                 <Button type="submit" variant="dark">Add + </Button>
                 <Button variant="info" type="reset">Reset</Button>
