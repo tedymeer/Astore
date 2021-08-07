5147f2f750101fcb55024c8c0fe49cbe3e3aa164
umair-teddy <umairhashmi517@gmail.com>
2021-08-08 02:45:08 +0500

    updated middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,8 +4,15 @@ module.exports.authenticate = async (req, res, next) => {
  //console.log("dhhdhahda")
   const token = req.cookies.JWTtoken;
   if(token){
-    const user = jwt.verify(token,process.env.JWT_SECRET);
-    req.user = user;
-    next();
+    try{
+      const user = jwt.verify("fef",process.env.JWT_SECRET);
+      req.user = user;
+      next();
+    }
+    catch(e){
+      res.status(400).json({
+        data:"you are not valid"
+      })
+    }
   }
 }
